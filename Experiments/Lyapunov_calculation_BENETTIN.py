7803a696349c75840a041e7c2670b73f915d3831
Kavya Tripathi <tripathikavya008@gmail.com>
2026-05-10 23:12:38 +0530

    finding lyapunov exponent using benettin

---
+++ Experiments/Lyapunov_calculation_BENETTIN.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+d_0 = 1e-8
+sum = 0.0
+n = 0.0
+d = pd.read_csv("Integrators\\lorenz_divergence_B.csv")
+
+for j in range(0,8000):
+    d_j = d.iloc[j,2]
+    g_k = np.log(d_j/d_0)
+    sum += g_k
+    n += 1
+
+
+dt = d.iloc[1,1] - d.iloc[0,1]
+lyap = sum / (n*dt)
+
+print('The lyapunov exponent is : ', lyap)
